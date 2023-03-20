1b429e4396887327b9b77b4be88fa548645dde3c
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-03-21 01:26:49 +0530

    21/3/23

---
+++ WT Lab/Testing/test1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.lang.Math;
+class test1{
+    public static void main(String[] args) {
+        int x = 64;
+        int y;
+        x = x << 2;
+        y = x >> 3;
+        System.out.println(x + " " + y);
+    }
+}
+
+// <link rel="stylesheet" type="text/css" href="mystyle.css">
+
