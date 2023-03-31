8f03da3e9204604cae59c37985aa60c44a87de66
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-03-31 13:17:37 +0530

    31/3/23

--- WT Lab/Java/Assignment 9/Q3_StringOperations.java
+++ WT Lab/Java/Assignment 9/Q3_StringOperations.java
@@ -13,30 +13,30 @@ public class Q3_StringOperations {
 		Scanner input = new Scanner(System.in);
 		String str1, str2;
 		
-		System.out.println("Enter a string: ");
+		System.out.print("Enter a string: ");
 		str1 = input.nextLine();
 		
 		// i) Changing case of string
-		System.out.println("String in uppercase: " + str1.ToUpperCase());
-		System.out.println("String in lowercase: " + str1.ToLowerCase());
+		System.out.println("String in uppercase: " + str1.toUpperCase());
+		System.out.println("String in lowercase: " + str1.toLowerCase());
 		
 		// ii) Reversing the string
 		StringBuffer reversedString = new StringBuffer(str1).reverse();
-		System.out.println("\nReversed String: " + reversedString);
+		System.out.println("Reversed String: " + reversedString);
 		
 		// iii) Comparing 2 strings
-		System.out.println("\nEnter another string: ");
+		System.out.print("\nEnter another string: ");
 		str2 = input.nextLine();
 		int flag = str1.compareTo(str2);
 		if (flag == 0)
 			System.out.println("The strings are the same.");
 		else if (flag == 1)
-			System.out.println(str1 + " is lexicographically greater than " + str2);
+			System.out.println(str2 + " is lexicographically greater than " + str1);
 		else
-			System.out.println(str2 + " is lexicographically greater than " + str2);
+			System.out.println(str1 + " is lexicographically greater than " + str2);
 		
 		/// iv) Insert one string into another string
-		System.out.println("\nEnter index at which to insert " + str2 + " into " + str1 + ": ");
+		System.out.print("\nEnter index at which to insert " + str2 + " into " + str1 + ": ");
 		int index = input.nextInt();
 		StringBuffer insertedString = new StringBuffer(str1);
 		insertedString.insert(index, str2);
