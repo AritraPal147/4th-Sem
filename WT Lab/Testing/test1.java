db185ce7175b2f81598200a8b79e489a637600b7
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-04-13 22:17:01 +0530

    13/4/23

--- WT Lab/Testing/test1.java
+++ WT Lab/Testing/test1.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
-import java.lang.Math;
 class test1{
     public static void main(String[] args) {
-        int x = 64;
-        int y;
-        x = x << 2;
-        y = x >> 3;
-        System.out.println(x + " " + y);
+        Null obj = new Null();
+        try {
+            System.out.println(obj.message( null));
+        } catch(NullPointerException e) {
+            System.out.println(e);
+        }
     }
 }
 
-// <link rel="stylesheet" type="text/css" href="mystyle.css">
+class Null {
+    public static Null initState() {
+        return null;
+    }
 
+    String message(String str) {
+        return str.toUpperCase();
+    }
+}
