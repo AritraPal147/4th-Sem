e354fc63730f9ddf027ad70ce7b01b4e25aa16a4
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-05-08 03:07:41 +0530

    8/5/23

---
+++ Exam/ShapesApplet.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.applet.*;
+
+public class ShapesApplet extends Applet {
+    public void paint(Graphics g) {
+        g.setColor(Color.red);
+        g.drawRect(10, 10, 50,50);
+        g.setColor(Color.cyan);
+        g.fillRect(10, 10, 50, 50);
+
+        g.setColor(Color.black);
+        g.drawOval(80, 80, 60, 60);
+    }
+}
+
+/* 
+    <applet code=ShapesApplet.java height=1000 width=1000>
+    </applet>
+*/
